bc6a2814b0110b669898c1a25339eeb70df2b396
Stefano Lusardi <lusardi.stefano@gmail.com>
2023-05-24 01:19:15 +0200

    Code cleanup. restored unit tests (FIXME!)

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,134 +1,45 @@
 #include <evds/event_dispatcher.hpp>
 #include <iostream>
 
-void foo(int i, int j)
-{
-    std::cout << "test - i: " << i << " j: " << j << std::endl;
-}
-
-template <typename T>
-void foo_t(T arg)
-{
-    std::cout << "foo_t " << arg << std::endl;
-}
-
-class Foo
-{
-public:
-    void bar(int i, std::string s)
-    {
-        std::cout << "test - i: " << i << " s: " << s << std::endl;
-    }
-
-    template <typename T>
-    void bar_t(T arg)
-    {
-        std::cout << "bar_t " << arg << std::endl;
-    }
-};
+using namespace std::chrono_literals;
 
 int main()
 {
-    using namespace std::chrono_literals;
+    evds::event_dispatcher e;
+    e.start(4);
 
-    evds::event_dispatcher evds;
-    evds.start();
+    /*
+    Does not compile:
+    e.add_handler<std::string>("event!", [](float arg){std::cout << arg << std::endl; });
 
-    // Handler names overloads:
-    // evds.add_event_handler("event_id_1", [] { std::cout << "event_id_1" << std::endl; });
-    // evds.add_event_handler("event_id_2", [] { std::cout << "event_id_2" << std::endl; });
+    static_assert:
+        event_handler arguments mismatch in add_handler()!
+        please match your handler arguments (input parameters) with your declaration (template specification)
+    */
 
-    // evds.push_event("event_id_1");
-    // evds.push_event("event_id_2");
+    /*
+    Does not compile:
+    e.add_handler<std::string>("event!", [](std::string arg){std::cout << arg << std::endl; return 42; });
 
-    // Parameter types overloads:    
-    // evds.add_event_handler("foo", [](const char* arg) { std::cout << "void(const char*) - " << arg << std::endl; });
-    evds.add_event_handler("foo", [](std::string arg) { std::cout << "void(std::string) - " << arg << std::endl; });
-    evds.add_event_handler("foo", [](std::string&& arg) { std::cout << "void(std::string&&) -" << arg << std::endl; });
-    // evds.add_event_handler("foo", [](const std::string& arg) { std::cout << "void(const std::string&) -  " << arg << std::endl; });
+    static_assert:
+        event_handler must return void!
+    */
 
-/*
-    std::cout << "---" << std::endl;
+    auto e1 = e.add_handler<std::string>("Event_A", [](const std::string& arg){ std::this_thread::sleep_for(1s); std::cout << arg + "1\n"; });
+    auto e2 = e.add_handler<std::string>("Event_A", [](const std::string& arg){ std::this_thread::sleep_for(1s); std::cout << arg + "2\n"; });
+    auto e3 = e.add_handler<std::string>("Event_A", [](const std::string& arg){ std::this_thread::sleep_for(1s); std::cout << arg + "3\n"; });
+    e.emit("Event_A", std::string("A"));
 
-    // evds.push_event("foo", "foo");
-    // evds.push_event<void, std::string>("foo", "foo");
-    // evds.push_event<void, std::string>("foo", std::string("foo"));
-    // evds.push_event<void, const std::string&>("foo", std::string("foo"));
-    evds.push_event<void, std::string&&>("foo", std::move(std::string("foo")));
+    bool r1 = e.remove_handler(e1); // true
+    bool r2 = e.remove_handler(e3); // true
+    bool r3 = e.remove_handler(0);  // false (id does not exists)
+    bool r4 = e.remove_handler(e3); // false (already removed)
+    e.emit("Event_A", std::string("A"));
 
-    // Return types overloads:
-    evds.add_event_handler("foo", [](int arg) -> void { std::cout << "void(int) - " << arg << std::endl; });
-    evds.add_event_handler("foo", [](int arg) -> int { std::cout << "int(int) - " << arg << std::endl; return arg; });
-    
-    const int i = 123;
-    evds.push_event<void, int>("foo", i);
-    evds.push_event<int, int>("foo", i);
+    // e.add_handler<float>("e2", [](float arg){std::cout << arg << std::endl; });
+    // e.add_handler<std::string>("e2", [](const std::string& arg){std::cout << arg << std::endl; });
 
-    // Custom objects:
-    evds.push_event("foo", Foo{});
-    
-    // Free function:
-    evds.add_event_handler("free_func", &foo);
-    const int free_func_int = 12345;
-    evds.push_event("free_func", free_func_int);
-    
-    // Free function template:
-    evds.add_event_handler("free_func_t", &foo_t<int>);
-    const int free_func_t_int = 12345;
-    evds.push_event("free_func_t", free_func_t_int);
-
-    // Member function with std::bind
-    Foo foo_obj;
-    std::function<void(int, std::string)> foo_bind = std::bind(&Foo::bar, &foo_obj, std::placeholders::_1, std::placeholders::_2);
-    evds.add_event_handler("foo_obj", std::move(foo_bind));
-
-    // Member function template with std::bind
-    std::function<void(std::string)> bar_bind = std::bind(&Foo::bar_t<std::string>, &foo_obj, std::placeholders::_1);
-    evds.add_event_handler("foo_obj_bar_t", std::move(bar_bind));
-    evds.push_event("foo_obj_bar_t", std::string("string-foo_obj_bar_t"));
-
-    //
-
-    std::this_thread::sleep_for(1s);
-
-    evds.push_event("bar");
-    std::this_thread::sleep_for(1s);
-
-    evds.push_event("foo", 1);
-    std::this_thread::sleep_for(1s);
-
-    evds.push_event<void, std::string>("foo", "string-2");
-    std::this_thread::sleep_for(1s);
-
-    evds.push_event("foo", Foo());
-    std::this_thread::sleep_for(1s);
-
-    evds.push_event("foo");
-    evds.remove_event_handler("foo");
-    evds.push_event("foo");
-    std::this_thread::sleep_for(1s);
-
-    auto func = [](int arg) { std::cout << "foo " << arg << std::endl; };
-    evds.add_event_handler("foo", std::move(func));
-
-    std::function<void(int, int)> func_free = foo;
-    evds.add_event_handler("foo", std::move(func_free));
-
-    std::function<void(int, int)> func_free2 = std::bind(foo, std::placeholders::_1, std::placeholders::_2);
-    evds.add_event_handler("foo", std::move(func_free2));
-
-    evds.add_event_handler("foo", &foo);
-
-*/
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
     std::cout << "FINISH!" << std::endl;
 
     return 0;
 }
-
-// Message Bus:
-// https://github.com/whichxjy/message-evds/blob/master/src/example.cpp
-
-// Function Traits:
-// https://functionalcpp.wordpress.com/2013/08/05/function-traits/
--- tests/src/test_lambda.cpp
+++ tests/src/test_lambda.cpp
@@ -1,7 +1,5 @@
 #include "test_event_dispatcher.hpp"
 #include <gtest/gtest.h>
-#include <thread>
-#include <type_traits>
 
 namespace evds::test
 {
@@ -25,72 +23,79 @@ TEST_F(evds_lambda, stop_twice)
 }
 
 // NOLINTNEXTLINE
-TEST_F(evds_lambda, add_event_handler)
+TEST_F(evds_lambda, add_handler)
 {
-    EXPECT_TRUE(e->add_event_handler("", [](){}));
+    EXPECT_TRUE(e->add_handler<void>("", [](){}));
 }
 
 // NOLINTNEXTLINE
 TEST_F(evds_lambda, remove_missing_event_handler)
 {
-    EXPECT_FALSE(e->remove_event_handler(""));
+    EXPECT_FALSE(e->remove_handler(0));
 }
 
 // NOLINTNEXTLINE
 TEST_F(evds_lambda, add_event_handler_twice_same_name)
 {
-    EXPECT_TRUE(e->add_event_handler("", [](){}));
-    EXPECT_FALSE(e->add_event_handler("", [](){}));
+    EXPECT_TRUE(e->add_handler("", [](){}));
+    EXPECT_FALSE(e->add_handler("", [](){}));
 }
 
 // NOLINTNEXTLINE
 TEST_F(evds_lambda, remove_event_handler_twice_same_name)
 {
-    EXPECT_FALSE(e->remove_event_handler(""));
-    EXPECT_FALSE(e->remove_event_handler(""));
+    EXPECT_FALSE(e->remove_handler(0));
+    EXPECT_FALSE(e->remove_handler(0));
 }
 
 // NOLINTNEXTLINE
 TEST_F(evds_lambda, add_event_handler_twice_different_name)
 {
-    EXPECT_TRUE(e->add_event_handler("1", [](){}));
-    EXPECT_TRUE(e->add_event_handler("2", [](){}));
+    EXPECT_TRUE(e->add_handler("1", [](){}));
+    EXPECT_TRUE(e->add_handler("2", [](){}));
 }
 
 // NOLINTNEXTLINE
 TEST_F(evds_lambda, remove_event_handler_after_add)
 {
-    EXPECT_TRUE(e->add_event_handler("", [](){}));
-    EXPECT_TRUE(e->remove_event_handler(""));
-    EXPECT_FALSE(e->remove_event_handler(""));
+    auto id = e->add_handler("", [](){});
+    EXPECT_EQ(id, 1);
+    EXPECT_TRUE(e->remove_handler(1));
+    EXPECT_FALSE(e->remove_handler(1));
 }
 
 // NOLINTNEXTLINE
 TEST_F(evds_lambda, mix_add_remove_event_handlers)
 {
-    EXPECT_TRUE(e->add_event_handler("1", [](){}));
-    EXPECT_TRUE(e->add_event_handler("2", [](){}));
-    EXPECT_FALSE(e->add_event_handler("2", [](){})); // already added
+    auto id1 = e->add_handler("1", [](){});
+    EXPECT_EQ(id1, 1);
 
-    EXPECT_TRUE(e->remove_event_handler("1"));
-    EXPECT_FALSE(e->remove_event_handler("1")); // already removed
+    auto id2 = e->add_handler("2", [](){});
+    EXPECT_EQ(id2, 2);
 
-    EXPECT_FALSE(e->remove_event_handler("3")); // never added
+    auto id3 = e->add_handler("2", [](){}); // already added
+    EXPECT_EQ(id3, 3);
+
+    EXPECT_TRUE(e->remove_handler(id1));
+    EXPECT_TRUE(e->remove_handler(id2));
+    EXPECT_FALSE(e->remove_handler(id2)); // already removed
+    EXPECT_FALSE(e->remove_handler(id3)); // never added
 }
 
 // NOLINTNEXTLINE
 TEST_F(evds_lambda, add_event_handler_after_start)
 {
     e->start();
-    EXPECT_TRUE(e->add_event_handler("", [](){}));
+    EXPECT_TRUE(e->add_handler("", [](){}));
 }
 
 // NOLINTNEXTLINE
 TEST_F(evds_lambda, remove_event_handler_after_start)
 {
     e->start();
-    EXPECT_TRUE(e->add_event_handler("", [](){}));
-    EXPECT_TRUE(e->remove_event_handler(""));
+    auto id = e->add_handler("", [](){});
+    EXPECT_EQ(id, 1);
+    EXPECT_TRUE(e->remove_handler(id));
 }
 
 // NOLINTNEXTLINE
@@ -98,7 +103,7 @@ TEST_F(evds_lambda, add_event_handler_after_stop)
 {
     e->start();
     e->stop();
-    EXPECT_TRUE(e->add_event_handler("", [](){}));
+    EXPECT_TRUE(e->add_handler("", [](){}));
 }
 
 
@@ -107,8 +112,9 @@ TEST_F(evds_lambda, remove_event_handler_after_stop)
 {
     e->start();
     e->stop();
-    EXPECT_TRUE(e->add_event_handler("", [](){}));
-    EXPECT_TRUE(e->remove_event_handler(""));
+    auto id = e->add_handler("", [](){});
+    EXPECT_EQ(id, 1);
+    EXPECT_TRUE(e->remove_handler(id));
 }
 
 // NOLINTNEXTLINE
@@ -120,7 +126,7 @@ TEST_F(evds_lambda, push_event_n_times_void_payload)
     
     const auto event_name = "EVENT_NAME";
     int count = -1;
-    e->add_event_handler(event_name, [&count, &cv]
+    e->add_handler(event_name, [&count, &cv]
     {
         count++;
         cv.notify_one();
@@ -144,7 +150,7 @@ TEST_F(evds_lambda, push_event_n_times_increasing_payload)
     
     const auto event_name = "EVENT_NAME";
     int count = -1;
-    e->add_event_handler(event_name, [&count, &cv](int expected_count_arg)
+    e->add_handler(event_name, [&count, &cv](int expected_count_arg)
     {
         count++;
         EXPECT_EQ(count, expected_count_arg);
@@ -169,7 +175,7 @@ TEST_F(evds_lambda, push_event_n_times_same_payload)
     const auto event_name = "EVENT_NAME";
     const int expected_value = 123456789;
     int count = -1;
-    e->add_event_handler(event_name, [&expected_value, &count, &cv](int expected_value_arg)
+    e->add_handler(event_name, [&expected_value, &count, &cv](int expected_value_arg)
     {
         count++;
         EXPECT_EQ(expected_value_arg, expected_value);
@@ -189,17 +195,17 @@ TEST_F(evds_lambda, push_event_n_times_same_payload)
 TEST_F(evds_lambda, add_same_name_multiple_handlers)
 {
     const auto event_name = "EVENT_NAME";
-    EXPECT_TRUE(e->add_event_handler(event_name, [](int i){}));
-    EXPECT_TRUE(e->add_event_handler(event_name, [](unsigned int i){}));
-    EXPECT_TRUE(e->add_event_handler(event_name, [](unsigned long i){}));
-    EXPECT_TRUE(e->add_event_handler(event_name, [](long long i){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](int i){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](unsigned int i){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](unsigned long i){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](long long i){}));
 
-    EXPECT_TRUE(e->add_event_handler(event_name, [](std::string s){}));
-    EXPECT_TRUE(e->add_event_handler(event_name, [](std::string& s){}));
-    EXPECT_TRUE(e->add_event_handler(event_name, [](std::string&& s){}));
-    EXPECT_TRUE(e->add_event_handler(event_name, [](const std::string& s){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](std::string s){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](std::string& s){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](std::string&& s){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](const std::string& s){}));
     
-    EXPECT_FALSE(e->add_event_handler(event_name, [](const std::string s){})); // same as non-const, non-ref
+    EXPECT_FALSE(e->add_handler(event_name, [](const std::string s){})); // same as non-const, non-ref
 }
 
 // NOLINTNEXTLINE
@@ -212,14 +218,14 @@ TEST_F(evds_lambda, push_multiple_handlers_same_event_name)
     const auto event_name = "EVENT_NAME";
 
     auto int_payload = 0;
-    EXPECT_TRUE(e->add_event_handler(event_name, [&cv, &int_payload](int i)
+    EXPECT_TRUE(e->add_handler(event_name, [&cv, &int_payload](int i)
     {
         int_payload = i;
         cv.notify_one();
     }));
 
     std::string str_payload = "";
-    EXPECT_TRUE(e->add_event_handler(event_name, [&cv, &str_payload](std::string s)
+    EXPECT_TRUE(e->add_handler(event_name, [&cv, &str_payload](std::string s)
     {
         str_payload = s;
         cv.notify_one();
@@ -248,7 +254,7 @@ TEST_F(evds_lambda, push_event_consecutive_start_stop)
     
     const auto event_name = "EVENT_NAME";
     int payload_value = 0;
-    e->add_event_handler(event_name, [&payload_value, &cv](int expected_value_arg)
+    e->add_handler(event_name, [&payload_value, &cv](int expected_value_arg)
     {
         payload_value = expected_value_arg;
         cv.notify_one();
@@ -278,7 +284,7 @@ TEST_F(evds_lambda, push_event_consecutive_start_stop)
     EXPECT_FALSE(e->push_event(event_name, expected_value3)); // handler not registered yet.
 
     // Add again the previous event handler since stop() clears all event handlers.
-    e->add_event_handler(event_name, [&payload_value, &cv](int expected_value_arg)
+    e->add_handler(event_name, [&payload_value, &cv](int expected_value_arg)
     {
         payload_value = expected_value_arg;
         cv.notify_one();
@@ -304,11 +310,11 @@ TEST_F(evds_lambda, push_event_with_different_overloads)
     e->start();
     const auto event_name = "EVENT_NAME";
 
-    EXPECT_TRUE(e->add_event_handler(event_name, [](float f1, float f2, float f3){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](float f1, float f2, float f3){}));
     EXPECT_TRUE((e->push_event<void, float, float, float>(event_name, 1.f, 2.f, 3.f)));
     EXPECT_FALSE((e->push_event<void, int, int, int>(event_name, 1, 2, 3))); // no match for int,int,int
 
-    EXPECT_TRUE(e->add_event_handler(event_name, [](int i1, int i2, int i3){}));
+    EXPECT_TRUE(e->add_handler(event_name, [](int i1, int i2, int i3){}));
     EXPECT_TRUE((e->push_event<void, int, int, int>(event_name, 1, 2, 3))); // now can match previous handler
 }
 
@@ -316,10 +322,10 @@ TEST_F(evds_lambda, push_event_with_different_overloads)
 TEST_F(evds_lambda, remove_event_with_arguments)
 {
     const auto event_name = "EVENT_NAME";
-    EXPECT_TRUE(e->add_event_handler(event_name, [](int i, std::string s){}));
-    EXPECT_FALSE((e->remove_event_handler<void, int>(event_name))); // no match void(int)
-    EXPECT_TRUE((e->remove_event_handler<void, int, std::string>(event_name))); // match: void(int, string)
-    EXPECT_FALSE((e->remove_event_handler<void, int, std::string>(event_name))); // already removed (previous line)
+    EXPECT_TRUE(e->add_handler(event_name, [](int i, std::string s){}));
+    EXPECT_FALSE((e->remove_handler<void, int>(event_name))); // no match void(int)
+    EXPECT_TRUE((e->remove_handler<void, int, std::string>(event_name))); // match: void(int, string)
+    EXPECT_FALSE((e->remove_handler<void, int, std::string>(event_name))); // already removed (previous line)
 }
 
 }
